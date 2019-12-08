99e678252b1de370d920b751554f1e65f2ac9421
Achsuthan <achsuthan@pikanite.com>
2019-12-08 19:34:06 +0530

    json file modified
    >>> ach

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ module.exports = {
   mode: "production",
   entry: "./src/components/ReactDropDown.jsx",
   output: {
-    path: path.resolve("lib"),
-    filename: "ReactDropDown.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve("dist"),
+    filename: "build.js",
+    libraryTarget: "umd"
   },
   module: {
     rules: [
