c1c187f4246e24177e5cb5eb78ff8a93551e5647
Achsuthan <achsuthan@pikanite.com>
2019-12-08 14:42:21 +0530

    1. roule modified
    >>> ach

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 use: 'babel-loader'
-            }
+            },
+            { test: /\.txt$/, use: 'raw-loader' }
         ]
     }
 }
