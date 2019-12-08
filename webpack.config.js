fce2e8bce1f01502feb7860df5a129386c6b9a13
Achsuthan <achsuthan@pikanite.com>
2019-12-08 16:52:43 +0530

    1. some file modified
    >>> ach

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,20 @@
-
-// webpack.config.js
 var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/components/ReactDropDown.jsx',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'ReactDropDown.jsx',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      }, {
-        test: /\.*css$/,
-        use : ExtractTextPlugin.extract({
-            fallback : 'style-loader',
-            use : [
-                'css-loader',
-                'sass-loader'
-            ]
-        })
-       },
-    ]
-  },
-  externals: {
-    'react': 'commonjs react' 
-  }
-};
+    mode: 'production',
+    entry: './src/components/ReactDropDown.jsx',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'ReactDropDown.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
