a7c839d9ff34640cf70bbe8e2e1c85c62cd26f06
Achsuthan <achsuthan@pikanite.com>
2019-12-08 14:57:09 +0530

    1. file modified
    >>> ach

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
-// webpack.config.js
 var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
@@ -21,16 +18,7 @@ module.exports = {
             presets: ['env']
           }
         }
-      }, {
-        test: /\.*css$/,
-        use : ExtractTextPlugin.extract({
-            fallback : 'style-loader',
-            use : [
-                'css-loader',
-                'sass-loader'
-            ]
-        })
-       },
+      }
     ]
   },
   externals: {
