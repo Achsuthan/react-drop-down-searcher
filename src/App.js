5f9cb466c795083ab1326a4483513656f960fac1
Achsuthan <achsuthan@pikanite.com>
2019-12-08 11:01:22 +0530

    1. handler function added
    >>> ach

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ReactDropDown from "./components/ReactDropDown";
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState([])
+  const list = [
+    {
+      name: "Testing 1",
+      value: "Testing 1"
+    },
+    {
+      name: "Testing 2",
+      value: "Testing 2"
+    },
+    {
+      name: "Testing 3",
+      value: "Testing 3"
+    }
+  ];
+  const toggleItem =(val)=>{
+    setSelectedItem(val)
+  }
+
   return (
-    <div className="text-center">
+    <div>
       <div className="col-6">
-        <ReactDropDown/>
+        <ReactDropDown placeholder="Type..." isSingle={false} list={list} labelName="name" selectedList ={selectedItem} selectedlabelName="name" toggleItem={(val)=> toggleItem(val)}/>
       </div>
     </div>
   );
