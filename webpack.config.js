fabfe846632818c70ba67180a14aac2e0e0396d6
Achsuthan <achsuthan@pikanite.com>
2019-12-08 15:01:10 +0530

    1. file modified
    >>> ach

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/components/ReactDropDown.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
+    filename: 'ReactDropDown.js',
     libraryTarget: 'commonjs2'
   },
   module: {
