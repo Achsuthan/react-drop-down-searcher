c8dfa198c7c333f5f53d8274de73487523d68306
Achsuthan <achsuthan@pikanite.com>
2019-12-08 12:14:21 +0530

    1. List value changed
    >>> ach

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ReactDropDown from "./components/ReactDropDown";
 
 function App() {
-  const [selectedItem, setSelectedItem] = useState([])
+  const [selectedItem, setSelectedItem] = useState([]);
   const list = [
     {
-      name: "Testing 1",
-      value: "Testing 1"
+      name: "End Game",
+      value: "End Game1"
     },
     {
-      name: "Testing 2",
-      value: "Testing 2"
+      name: "The Lion King",
+      value: "The Lion King"
     },
     {
-      name: "Testing 3",
-      value: "Testing 3"
+      name: "Spider-Man: Far From Home",
+      value: "Spider-Man: Far From Home"
+    },
+    {
+      name: "Captain Marvel",
+      value: "Captain Marvel"
+    },
+    {
+      name: "Jorker",
+      value: "Jorker"
     }
   ];
-  const toggleItem =(val)=>{
-    setSelectedItem(val)
+  const toggleItem = val => {
+    setSelectedItem(val);
+  };
+  const handleInputChange= (val)=>{
+
   }
 
   return (
-    <div>
-      <div className="col-6">
-        <ReactDropDown placeholder="Type..." isSingle={false} list={list} labelName="name" selectedList ={selectedItem} selectedlabelName="name" toggleItem={(val)=> toggleItem(val)}/>
+    <div className="container">
+      <br/>
+      <br/>
+      <br/>
+      <div>
+        <ReactDropDown
+          placeholder="Dropdown..."
+          isSingle={true}
+          list={list}
+          labelName="name"
+          selectedList={selectedItem}
+          selectedlabelName="name"
+          toggleItem={val => toggleItem(val)}
+          handleInputChange= {(val)=>handleInputChange(val)}
+        />
       </div>
     </div>
   );
